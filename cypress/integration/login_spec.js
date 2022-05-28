b6830a5bfd3d9b086c5243b8fb064947999eea58
ismail eyamba <vheyam@hotmail.com>
2022-05-28 17:46:45 +0100

    cypress tests to populate email and password fields on homepage

---
+++ cypress/integration/login_spec.js
@@ -0,0 +1,27 @@
+describe('Input form', () => {
+    beforeEach(() =>{
+        cy.visit('/')
+    })
+
+    it('focuses input on load', () => {
+      
+      cy.focused()
+        .should('have.class', 'emailinputtest')
+    })
+    
+    it('accepts input',() =>{
+        const typedText ='vheyam@hotmail.com'
+    
+        cy.get('.emailinputtest')
+        .type(typedText)
+        .should('have.value', typedText)
+   
+        const typedText2 ='ismail'
+    
+        cy.get('.passwordinputtest')
+        .type(typedText2)
+        .should('have.value', typedText2)
+        
+    })
+    
+  })
